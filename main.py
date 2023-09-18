1f79042258cef7541f219aebf23ae31314fc7205
Levia Yang <73449544+xxxtesttest@users.noreply.github.com>
2023-09-17 22:44:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import polars as pl
 import matplotlib.pyplot as plt
 import os
+from polars.errors import EmptyDataFrame, PolarsError
 
 # Function to calculate statistics for specific columns
 def calculate_statistics(file_path):
@@ -10,7 +11,10 @@ def calculate_statistics(file_path):
 
         # Selecting specific columns of interest
         selected_columns = ['danceability', 'energy', 'artist_popularity', 'loudness']
-        data = data[selected_columns]
+        data = data.select(selected_columns)
+
+        if data.is_empty():
+            raise EmptyDataFrame("The DataFrame is empty")
 
         # Calculating mean, median
         mean = data.mean()
@@ -20,16 +24,23 @@ def calculate_statistics(file_path):
         median = median.round(1)
 
         return {'mean': mean, 'median': median}
-    except pl.errors.EmptyDataError as e:
+    except EmptyDataFrame as e:
+        return str(e)
+    except PolarsError as e:
         return str(e)
 
 # Function to visualize specific columns as histograms
 def visualize_data(file_path, save_path=None):
     try:
-        # Check if the input is a DataFrame
+        # Reading the dataset from the CSV file
         data = pl.read_csv(file_path)
-        if not isinstance(data, pl.DataFrame):
-            raise ValueError("Input is not a Polar DataFrame")
+
+        # Selecting specific columns of interest
+        selected_columns = ['danceability', 'energy', 'artist_popularity', 'loudness']
+        data = data.select(selected_columns)
+
+        if data.is_empty():
+            raise EmptyDataFrame("The DataFrame is empty")
 
         # Create a directory to store the plots if save_path is provided
         if save_path:
@@ -39,7 +50,7 @@ def visualize_data(file_path, save_path=None):
         histogram_paths = []
         for col in data.columns:
             plt.figure(figsize=(8, 6))
-            plt.hist(data[col], bins=20, edgecolor='k', alpha=0.7)
+            plt.hist(data[col].to_list(), bins=20, edgecolor='k', alpha=0.7)
             plt.xlabel(col)
             plt.ylabel("Frequency")
             plt.title(f"Histogram of {col}")
@@ -55,7 +66,9 @@ def visualize_data(file_path, save_path=None):
 
         if save_path:
             return histogram_paths
-    except ValueError as e:
+    except EmptyDataFrame as e:
+        return str(e)
+    except PolarsError as e:
         return str(e)
 
 # Function to calculate the correlation of artist_popularity with other columns
@@ -66,7 +79,10 @@ def calculate_correlation(file_path):
 
         # Selecting specific columns of interest
         selected_columns = ['danceability', 'energy', 'artist_popularity', 'loudness']
-        data = data[selected_columns]
+        data = data.select(selected_columns)
+
+        if data.is_empty():
+            raise EmptyDataFrame("The DataFrame is empty")
 
         # Calculating the correlation matrix
         correlation_matrix = data.corr()
@@ -75,22 +91,7 @@ def calculate_correlation(file_path):
         artist_popularity_correlation = correlation_matrix['artist_popularity']
 
         return artist_popularity_correlation
-    except pl.errors.EmptyDataError as e:
+    except EmptyDataFrame as e:
         return str(e)
-
-if __name__ == "__main__":
-    dataset_path = "playlist.csv"
-
-    # Calculate statistics for specific columns
-    statistics_result = calculate_statistics(dataset_path)
-    print("Descriptive Statistics:")
-    print(statistics_result)
-
-    # Visualize specific columns as histograms
-    # To save the plots as images in a 'plots' directory
-    visualize_data(dataset_path, save_path='plots')
-
-    # Calculate and print the correlation of artist_popularity with other columns
-    correlation_result = calculate_correlation(dataset_path)
-    print("\nCorrelation of artist_popularity with other columns:")
-    print(correlation_result)
+    except PolarsError as e:
+        return str(e)
